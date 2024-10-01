8ef364beffa4bf1f0967f04c9afd7e05f875682f
Saurabh <idkxyz001@gmail.com>
2024-10-02 00:52:52 +0530

    Initial commit

---
+++ precache-manifest.4088508310394e8c990038445223609e.js
@@ -0,0 +1,302 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1e50f00beb15a077da03fd62c6ae4c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96b4f2e828e8ad925db8",
+    "url": "/static/css/main.0d28778b.chunk.css"
+  },
+  {
+    "revision": "05e1f2f9ae22da08baca",
+    "url": "/static/js/2.8ccaa36c.chunk.js"
+  },
+  {
+    "revision": "96b4f2e828e8ad925db8",
+    "url": "/static/js/main.7e3607d0.chunk.js"
+  },
+  {
+    "revision": "bd3e6416c025eceacc49",
+    "url": "/static/js/runtime~main.663dd8c0.js"
+  },
+  {
+    "revision": "7437eafe77bb7fc55ce5b52ca097f025",
+    "url": "/static/media/1.7437eafe.jpg"
+  },
+  {
+    "revision": "1dc4922eb5a12d6ba3b8801fc336d841",
+    "url": "/static/media/11.1dc4922e.jpg"
+  },
+  {
+    "revision": "369996ad2664692dc7e9b01131ddbc88",
+    "url": "/static/media/12.369996ad.jpg"
+  },
+  {
+    "revision": "3645db821609e38fdff60fb86e4b7538",
+    "url": "/static/media/2.3645db82.jpg"
+  },
+  {
+    "revision": "330b9d35c15d876374b661c0dc9fae43",
+    "url": "/static/media/3.330b9d35.jpg"
+  },
+  {
+    "revision": "923394a747750fc2b46eb09cd104719d",
+    "url": "/static/media/4.923394a7.jpg"
+  },
+  {
+    "revision": "fba34964c2ac63b5712c3c45db7e24e6",
+    "url": "/static/media/6.fba34964.jpg"
+  },
+  {
+    "revision": "71216eaaab708ea28f53a0d9f09123d1",
+    "url": "/static/media/7.71216eaa.jpg"
+  },
+  {
+    "revision": "c127fb2f9417e0a7832deaac563a821c",
+    "url": "/static/media/8.c127fb2f.jpg"
+  },
+  {
+    "revision": "2b8c120b4961ca309394ed3089b10a62",
+    "url": "/static/media/9.2b8c120b.jpg"
+  },
+  {
+    "revision": "caa0edb5ec32b8cbb2013ebf64bfdfd3",
+    "url": "/static/media/BGVideo.caa0edb5.mp4"
+  },
+  {
+    "revision": "78960249247adb01d6d0d99cb187b41d",
+    "url": "/static/media/BGvedio1.78960249.mp4"
+  },
+  {
+    "revision": "d01f6a524827d080143ba7b6a4fd2961",
+    "url": "/static/media/Cart.d01f6a52.png"
+  },
+  {
+    "revision": "a7b369a6b3f77afbc8f1ae9e220ceeec",
+    "url": "/static/media/Colorful_Cartoon_Cats_Sweatshirt.a7b369a6.jpg"
+  },
+  {
+    "revision": "9d73d8816f327d77e24194fa58c26eb1",
+    "url": "/static/media/Confident Casual in Contemporary Striped Sweatshirt.9d73d881.jpg"
+  },
+  {
+    "revision": "5d1aa324706d09cc701409e5315c335b",
+    "url": "/static/media/Dynamic Sketches Sports Illustrated Sweatshirt.5d1aa324.jpg"
+  },
+  {
+    "revision": "7714050178bf6299de9a5bd15428425b",
+    "url": "/static/media/Ethereal Glide White Sneakers.77140501.jpg"
+  },
+  {
+    "revision": "e45ebd5258d77392cb78ceb7b3133eeb",
+    "url": "/static/media/Github.e45ebd52.png"
+  },
+  {
+    "revision": "bce3f5faab1b7618f9eda496ea7f23c8",
+    "url": "/static/media/Hero02.bce3f5fa.jpg"
+  },
+  {
+    "revision": "4ab8eab0e7163e3e03ac5573caec51c9",
+    "url": "/static/media/Hero03.4ab8eab0.png"
+  },
+  {
+    "revision": "4170ebe650dccbabf43200b609182afb",
+    "url": "/static/media/Hero1.4170ebe6.jpg"
+  },
+  {
+    "revision": "079e0ac706226aafa23971060c9d7c22",
+    "url": "/static/media/Hero4.079e0ac7.jpg"
+  },
+  {
+    "revision": "df5a0f0c39db7debb2654bbab88fe86f",
+    "url": "/static/media/Hero5.df5a0f0c.jpg"
+  },
+  {
+    "revision": "f21a6f45ffd52ca533a032fe797d5f6a",
+    "url": "/static/media/Hero6.f21a6f45.jpg"
+  },
+  {
+    "revision": "93172dfdedd298388144107b729b0fc9",
+    "url": "/static/media/Hero7.93172dfd.jpg"
+  },
+  {
+    "revision": "900b1094c90e3d135782ae9495629d0d",
+    "url": "/static/media/Hero8.900b1094.jpg"
+  },
+  {
+    "revision": "8085cee7ab462d5e17b7b54bda546051",
+    "url": "/static/media/Hero9.8085cee7.jpg"
+  },
+  {
+    "revision": "9bc682be5882492a0bf0bbef1bd4134c",
+    "url": "/static/media/Instagram.9bc682be.png"
+  },
+  {
+    "revision": "968ea62882943e88bbd318ae5fa67429",
+    "url": "/static/media/Linkedin.968ea628.png"
+  },
+  {
+    "revision": "e76c6e7ea95ea00b98f784bb8a316b2f",
+    "url": "/static/media/Midnight Coders.e76c6e7e.jpg"
+  },
+  {
+    "revision": "85712b895d99b9008f2a5cb1774aa0c4",
+    "url": "/static/media/Minimalist Composition with Vase and Dried Plants.85712b89.jpg"
+  },
+  {
+    "revision": "ab55c6f789841295a1a03c3b3f325a1b",
+    "url": "/static/media/Minimalist Elegance in Cream and Navy.ab55c6f7.jpg"
+  },
+  {
+    "revision": "bb1e22105b7c9a0fcdc90aab2839fefa",
+    "url": "/static/media/New White Sneakers on Pink Background.bb1e2210.jpg"
+  },
+  {
+    "revision": "5c225f97ae516a06c77e5fa6b9935b4d",
+    "url": "/static/media/Pristine White Sneakers (1).5c225f97.jpg"
+  },
+  {
+    "revision": "fbd352583ddd1c4da08c592a0c2c6d97",
+    "url": "/static/media/Pristine White Sneakers and Blue Jeans.fbd35258.jpg"
+  },
+  {
+    "revision": "b1cadfd4a22eff565531aeaf63454f59",
+    "url": "/static/media/Pristine White Sneakers on Aqua Blue.b1cadfd4.jpg"
+  },
+  {
+    "revision": "65e52593a752eade633c7281ddef8ccb",
+    "url": "/static/media/Pristine White Sneakers on Gray Surface.65e52593.jpg"
+  },
+  {
+    "revision": "fca82049df1fd149731dc02e4779dba1",
+    "url": "/static/media/Pristine White Sneakers with Soft Shadows.fca82049.jpg"
+  },
+  {
+    "revision": "5e9352504740e665f77a251eaee8fe5c",
+    "url": "/static/media/Pristine White Sneakers.5e935250.jpg"
+  },
+  {
+    "revision": "894a2ede85a483bf9bedefd4db45cdb9",
+    "url": "/static/media/Roboto-Medium.894a2ede.ttf"
+  },
+  {
+    "revision": "df7b648ce5356ea1ebce435b3459fd60",
+    "url": "/static/media/Roboto-Regular.df7b648c.ttf"
+  },
+  {
+    "revision": "0e54e9af8a1c9ac6e2bd1fc847408516",
+    "url": "/static/media/SellenaBrush-x3JyK.0e54e9af.ttf"
+  },
+  {
+    "revision": "1cc2c8656d41c73c8793ddc0ae9c2209",
+    "url": "/static/media/Serene Minimalist Plant.1cc2c865.jpg"
+  },
+  {
+    "revision": "96367c2083888cbc2d08e5a18405f9be",
+    "url": "/static/media/Serene Woman in Monochrome Attire.96367c20.jpg"
+  },
+  {
+    "revision": "b4ae06e1dc94a9c3282d484afbea646b",
+    "url": "/static/media/Sunlit Workspace with Plants and Laptop.b4ae06e1.jpg"
+  },
+  {
+    "revision": "2c6e94aaf20d66610132b533ae100324",
+    "url": "/static/media/User.2c6e94aa.png"
+  },
+  {
+    "revision": "31db185ea5a14b1452721ec62313b8f0",
+    "url": "/static/media/Vibrant Footwear Showcase.31db185e.jpg"
+  },
+  {
+    "revision": "535aef2192da3448a0a5fe0c1c136ebe",
+    "url": "/static/media/Vibrant_Orange_Crewneck_Sweater.535aef21.jpg"
+  },
+  {
+    "revision": "90d6487e1322e16b47b11e2121e7494d",
+    "url": "/static/media/Vintage Shield Graphic Crew Neck Sweatshirt in Light Grey.90d6487e.jpg"
+  },
+  {
+    "revision": "b39850fee97a270f4e204ab6be6cde89",
+    "url": "/static/media/Vintage_Olive Crewneck_with Leather_Shield Patch.b39850fe.jpg"
+  },
+  {
+    "revision": "dec5b3ca3fdc305985e1506daa1d93f0",
+    "url": "/static/media/White Sneakers and Blue Jeans Street Style.dec5b3ca.jpg"
+  },
+  {
+    "revision": "943360747cfcd70c8312f9c1dc365f20",
+    "url": "/static/media/background2.94336074.mp4"
+  },
+  {
+    "revision": "ac88a2ad638199f7be8e79cb88dfc662",
+    "url": "/static/media/f1.ac88a2ad.jpg"
+  },
+  {
+    "revision": "fa575062bf0a3d735a48d5289417b133",
+    "url": "/static/media/f2.fa575062.jpg"
+  },
+  {
+    "revision": "da57137c6d02511624c4b1331b75eb71",
+    "url": "/static/media/f3.da57137c.jpg"
+  },
+  {
+    "revision": "bb00c898ffa754ec7b0edf7be789e4bb",
+    "url": "/static/media/f4.bb00c898.jpg"
+  },
+  {
+    "revision": "50bd3d150a2cd8a27964c942d31ea2f0",
+    "url": "/static/media/f5.50bd3d15.jpg"
+  },
+  {
+    "revision": "98b50b8386fac8bfe92e36b24d99c3c5",
+    "url": "/static/media/f6-2.98b50b83.jpg"
+  },
+  {
+    "revision": "f3889da3cf8f1b6b2d6ef58491f88641",
+    "url": "/static/media/f6.f3889da3.jpg"
+  },
+  {
+    "revision": "d371ff81be8a2168f46bb8cf59e6f1f7",
+    "url": "/static/media/f7.d371ff81.jpg"
+  },
+  {
+    "revision": "431a9c64b2bf192c4bf32a0fac508521",
+    "url": "/static/media/n1.431a9c64.jpg"
+  },
+  {
+    "revision": "a386b88ac2527590b21b3efec9560cf4",
+    "url": "/static/media/n2.a386b88a.jpg"
+  },
+  {
+    "revision": "ae2f9877643cf8d8b36d83cc645e8f1f",
+    "url": "/static/media/n3.ae2f9877.jpg"
+  },
+  {
+    "revision": "d1f8ca2bbd45d493f1946ba21293020a",
+    "url": "/static/media/n4.d1f8ca2b.jpg"
+  },
+  {
+    "revision": "38332293fac6fc8cdfdc3ed3ea4f7015",
+    "url": "/static/media/n5.38332293.jpg"
+  },
+  {
+    "revision": "99ec3409047b77dac934ed5484030ab0",
+    "url": "/static/media/n6.99ec3409.jpg"
+  },
+  {
+    "revision": "79c101d857bf535714c9e18e39c376a7",
+    "url": "/static/media/n7.79c101d8.jpg"
+  },
+  {
+    "revision": "9e0b8f03d165f98c937caa50129eb837",
+    "url": "/static/media/n8.9e0b8f03.jpg"
+  },
+  {
+    "revision": "ab2c55615667020d441afa656826b99c",
+    "url": "/static/media/profile.ab2c5561.jpg"
+  },
+  {
+    "revision": "191abd1e00daa1658a2a9c6a59a9a028",
+    "url": "/static/media/star.191abd1e.png"
+  }
+]);
